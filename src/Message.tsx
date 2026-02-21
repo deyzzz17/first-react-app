97ee6d78f003f03f282589272feb986fd6e7b3d6
deyzzz17 <swodey@free.fr>
2026-02-21 20:48:20 +0100

    update

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,4 @@
-function Message() {
-  const name = "Sophie";
+function Message(name: string) {
   if (name) return <h1>Hello {name}</h1>;
   return <h1>Hello World</h1>;
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,38 @@
-export function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+import { useState } from "react";
 
-  if (items.length == 0) return <p>No item found</p>;
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
   );
 }
+
+export default ListGroup;
